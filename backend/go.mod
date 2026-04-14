bdb804b5263b75d4a87269951d3594c8ab222b29
Daniel Velez <functionitservices@gmail.com>
2026-04-14 16:19:05 -0500

    feat: initialize Go module and project skeleton

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/court-command/court-command
+
+go 1.26.2
---
+++ backend/main.go
@@ -0,0 +1,12 @@
+// backend/main.go
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Court Command v2 starting...")
+	os.Exit(0)
+}
